1b00eec860af6f8ac79af54ce425052b5091ad83
saintClever <parchment@gmx.com>
2019-08-04 06:07:32 -0400

    sql databe initial migration

--- guestbook.py
+++ guestbook.py
@@ -1,14 +1,49 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    comment = db.Column(db.String(1000))
 
 @app.route('/')
 def index():
-    return '<h1>Hello There!</h1>'
+    result = Comment.query.all()
+    return render_template('index.html', result=result)
 
-@app.route('/home')
-def home():
-    return '<h1>You are on the home page</h1>'
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+
+    signature = Comment(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+
+    # return 'Name is: ' + name + ' and the comment is: ' + comment ## TEST in process
+    # return render_template('index.html', name=name, comment=comment)
+    return redirect(url_for('index'))
+
+# @app.route('/home', methods=['GET', 'POST'])
+# def home():
+#     links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org', 'https://www.enkato.com']
+#     return render_template('example.html', myvar='Flask example', links=links)
 
 if __name__ == '__main__':
     app.run(debug=True)
